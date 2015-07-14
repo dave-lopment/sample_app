f7fad5eedab2f2b9265a9b629adccb164b990b81
David Landry <dlandry@avarteq.de>
2015-07-14 09:50:29 +0200

    Finish log in / log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+
+    # assert_select 'div#error_explanation',
+    # assert_select 'div.<CSS class for field with error>'
+
+
   end
 
   test "valid signup information" do
@@ -22,5 +27,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
+    # assert_not flash.empty?
   end
 end
