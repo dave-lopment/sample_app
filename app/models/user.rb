a68f75d32e87e8a96e178aaaeb33d9093117dc7a
David Landry <dlandry@avarteq.de>
2015-07-15 11:10:24 +0200

    add account activation attributes to database (and to users.yml and seeds.db)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -10,7 +12,7 @@ class User < ActiveRecord::Base
     uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil:true 
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil:true
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -38,4 +40,17 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
