31640f49f6fe41121eda838eae12c534c334e2fa
David Landry <dlandry@avarteq.de>
2015-07-15 10:41:29 +0200

    site layout test angepasst

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,6 +13,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "links should be correct for logged in and non logged in users" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", users_path, count: 0
+    
+    # log_in_as(@user)
+    # assert_select "a[href=?]", login_path, count: 0
+    # assert_select "a[href=?]", users_path
+    # assert_select "a[href=?]", edit_user_path(@user.id)
+
   end
 
   # test "exercise-test" do
