d6dc958541c5e23f386fdd1da97279cd7b569f41
David Landry <dlandry@avarteq.de>
2015-07-16 08:18:39 +0200

    add Password reset functuinality, just before chapter 10.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,12 +57,17 @@ class User < ActiveRecord::Base
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
-    update_atribute(:reset_sent_at, Time.zone.now)
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
-  end 
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
 
